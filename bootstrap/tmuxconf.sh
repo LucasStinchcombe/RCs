f5cebd45c03ae4d377df7b8aa9afc8e0ad95cd8b
onetwo <lucasstinchcombe@gmail.com>
2019-01-21 21:09:48 +0900

    Add tmux.conf bootstrap

---
+++ bootstrap/tmuxconf.sh
@@ -0,0 +1,13 @@
+# bootstrap tmux.conf
+
+rc_install()
+{
+    # symlink vimrc
+    echo -n "Symlinking tmux.conf: "
+    if [ -f ~/.tmux.conf ]; then
+        echo "Skip"
+    else
+        ln -s $RC_FILES_DIR/tmux.conf ~/.tmux.conf
+        echo "Done"
+    fi
+}
