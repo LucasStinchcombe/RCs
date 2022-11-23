e5bd0edb64af4eafc0ab5edcf27d1cceb77eca3d
Lucas Stinchcombe <lucasstinchcombe@gmail.com>
2022-11-23 08:51:48 -0800

    Add ultisnips bootstrap

---
+++ bootstrap/ultisnips.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+rc_install()
+{
+    echo -n "Adding ultisnips: "
+
+    # Check if ~/.vim/Ultisnips exists
+    if [ -d "$HOME/.vim/UltiSnips" ]
+    then
+        echo "Skip"
+    else
+        ln -s "$RC_FILES_DIR/UltiSnips" "$HOME/.vim/UltiSnips"
+        echo "Done"
+    fi
+}
